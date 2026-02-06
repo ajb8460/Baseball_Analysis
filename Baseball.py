20f766b3dabe6d35d70475996e2adf9536cfebc8
ajb8460 <ajb8460@psu.edu>
2026-02-06 18:33:27 -0500

    Started data collection

--- Baseball.py
+++ Baseball.py
@@ -1,19 +1,23 @@
-'''from pybaseball import batting_stats
+from pybaseball import batting_stats
 from pybaseball import pitching_stats
 from pybaseball import statcast
 
 import pandas as pd
 
 
-Batting = batting_stats(2025)
+Batting = batting_stats(2024)
 
 Pitching = pitching_stats(2024)
 
-good_players = Batting[Batting['WAR'] >= 3]
-'''
-import numpy as np
-x = float(10**17)
-y=np.spacing(x)
-print(x)
+#good_players = Batting[Batting['WAR'] >= 3]
 
-#So will this auto save now?
+#print(Batting)
+
+from pybaseball import teamid_lookup
+
+abbrevs = teamid_lookup()['team_abbrev']
+
+
+'''from pybaseball import standings
+data = standings(2016)[1]
+print(data)'''
