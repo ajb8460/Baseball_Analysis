dcaa60b50ecd604ba311e3d7e583fd581ae7000c
Alex Bechakas <166137148+ajb8460@users.noreply.github.com>
2026-02-06 17:39:13 -0500

    Initial commit for document

---
+++ Baseball.py
@@ -0,0 +1,17 @@
+'''from pybaseball import batting_stats
+from pybaseball import pitching_stats
+from pybaseball import statcast
+
+import pandas as pd
+
+
+Batting = batting_stats(2025)
+
+Pitching = pitching_stats(2024)
+
+good_players = Batting[Batting['WAR'] >= 3]
+'''
+import numpy as np
+x = float(10**17)
+y=np.spacing(x)
+print(x)
